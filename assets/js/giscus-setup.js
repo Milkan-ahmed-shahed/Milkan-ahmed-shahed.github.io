b65ecbe31344d8888627cfc5023ad9881c65d67d
Milkan-ahmed-shahed <mshahed221063@bseee.uiu.ac.bd>
2026-03-05 12:39:10 +0000

    Deploying to gh-pages from @ Milkan-ahmed-shahed/Milkan-ahmed-shahed.github.io@2a097d7a464d3db3e2c3ebaf24bd205689e1fd50 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Milkan-ahmed-shahed/Milkan-ahmed-shahed.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
